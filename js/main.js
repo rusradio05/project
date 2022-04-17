13a3b4d59f26f1c24663bedf4d4afede5676deca
rusradio05 <rusradio05@gmail.com>
2022-04-17 19:26:49 +0300

    creating my first commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const num = 50;
+switch (num) {
+  case 30:
+    console.log("err-");
+    break;
+  case 50:
+    console.log("err+");
+    break;
+  case 40:
+    console.log("yes");
+    break;
+  default:
+    console.log("next time");
+    break;
+}
