e5c5bca8796b915c0e58eab93bc60a37f5a4a375
rusradio05 <rusradio05@gmail.com>
2022-04-18 10:07:24 +0300

    21. Логические операторы

--- js/main.js
+++ js/main.js
@@ -1,17 +1,78 @@
 "use strict";
 
-const num = 50;
-switch (num) {
-  case 30:
-    console.log("err-");
-    break;
-  case 50:
-    console.log("err+");
-    break;
-  case 40:
-    console.log("yes");
-    break;
-  default:
-    console.log("next time");
-    break;
+// // const hamb = 3,
+// //   fries = 5,
+// //   cola = 0;
+
+// // console.log(hamb >= 2 && cola && fries);
+
+// // if (hamb >= 3 && cola && fries) {
+// //   console.log("Enter");
+// // } else {
+// //   console.log("Exit");
+// // }
+// // const a = undefined;
+
+// console.log(1 && 0);
+// console.log(1 && 5);
+// console.log(null && 5);
+// console.log(" " && "dagfhj");
+
+// const hamb = 3,
+//   fries = 1,
+//   cola = 0;
+
+// console.log(cola || fries || hamb);
+
+// if (hamb || cola || fries) {
+//   console.log("Enter");
+// } else {
+//   console.log("Exit");
+// }
+
+// let johnRep,
+//   maxRep,
+//   andyRep = "s",
+//   annRep = "done";
+// console.log(johnRep || maxRep || andyRep || annRep);
+
+// const hamb = 3,
+//   fries = 3,
+//   cola = 1,
+//   nuggets = 1;
+
+// if ((hamb === 3 && cola == 2) || (fries === 3 && nuggets)) {
+//   console.log("Enter");
+// } else {
+//   console.log("Exit");
+// }
+// console.log(hamb === 3 && cola == 2);
+// console.log(fries === 3 && nuggets);
+// console.log(hamb === 3 && (cola == 2 || fries === 3) && nuggets);
+
+// console.log(!!1);
+
+// console.log(NaN || 2 || undefined);
+
+// console.log(NaN && 2 && undefined);
+
+// console.log(1 && 2 && 3);
+
+// console.log((!1 && 2) || !3);
+
+// console.log(25 || (null && !3));
+
+// console.log(NaN || null || !3 || undefined || 5);
+
+// console.log(!5 && undefined && null && !3); // 5
+
+// console.log((5 === 5 && 3 > 1) || 5); // true
+
+const hamburger = 3;
+const fries = 3;
+const cola = 0;
+const nuggets = 2;
+
+if ((hamburger === 3 && cola) || (fries === 3 && nuggets)) {
+  console.log("Done!");
 }
