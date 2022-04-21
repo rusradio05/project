4ca5cbad392dfe85462cdf72c3d4c63e96a37c27
rusradio05 <rusradio05@gmail.com>
2022-04-21 15:54:15 +0300

    решил упр. по написанию кода №6

--- js/script.js
+++ js/script.js
@@ -28,7 +28,81 @@
 // function foo (){
 // // code
 // };
-foo();
-function foo() {
-  console.log("Hello");
+// foo();
+// function foo() {
+//   console.log("Hello");
+// }
+// const usdCurr = 28;
+// const eurCurr = 32;
+// const discount = 0.9;
+
+// function convert(amount, curr) {
+//   return amount * curr;
+// }
+
+// function promotion(result) {
+//   console.log(result * discount);
+// }
+
+// promotion(convert(1000, usdCurr));
+
+// создаем функцию, которая конвертирует сумму curr по определенному курсу валют $Curr
+// затем создаем функцию, которая продает валюту со скидкой 10%
+
+const usdCurr = 28;
+const eurCurr = 32;
+const discount = 0.9;
+
+function convert(amount, curr) {
+  return amount * curr;
 }
+
+function promotion(result) {
+  console.log(result * discount);
+}
+
+const resUsd = convert(1000, usdCurr);
+const resEur = convert(1000, eurCurr);
+
+console.log(resUsd);
+console.log(resEur);
+promotion(resUsd);
+promotion(resEur);
+
+function test() {
+  for (let i = 0; i < 5; i++) {
+    console.log(i);
+    if (i === 3) {
+      return;
+    }
+  }
+  console.log("Done!");
+}
+test();
+
+function sayHello(userName) {
+  return "Привет, " + userName + "!";
+}
+console.log(sayHello("Gena"));
+
+function returnNeighboringNumbers(num) {
+  let mass = [];
+  mass[0] = num - 1;
+  mass[1] = num;
+  mass[2] = num + 1;
+  return mass;
+}
+console.log(returnNeighboringNumbers(5));
+
+function getMathResult(num, count) {
+  let str = "";
+  for (let i = 1; i <= count; i++) {
+    if (i < count) {
+      str += num * i + "---";
+    } else if (i == count) {
+      str += num * i;
+    }
+  }
+  return str;
+}
+console.log(getMathResult(10, 5));
